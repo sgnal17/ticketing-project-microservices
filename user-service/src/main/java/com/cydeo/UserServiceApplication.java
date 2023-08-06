f3a2a0ff629d2d0fab33ce6d08afc9d36c107604
sgnal17 <ysfackgz68@gmail.com>
2023-08-06 19:12:48 -0400

    config added

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +12,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {
+        @Server(url = "/user-service")
+},info =
+@Info(title = "Ticketing App",version = "1.0", description = "User Service API"))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
